b24ff5bc8e63d5d9927a77cbca3b8413930fa463
abdalla114 <108231471+abdalla114@users.noreply.github.com>
2024-05-20 17:09:20 +0300

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppointmentComponent } from './components/appointment/appointment.component';
+import { LoginComponent } from './components/login/login.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http'; // Add this line
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCardModule} from '@angular/material/card';
+import {MatNativeDateModule} from '@angular/material/core';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentComponent,
+    LoginComponent,
+    OverviewComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule, 
+    MatDatepickerModule,
+    MatCardModule,
+    MatNativeDateModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
